43137af2fb29e4cf2be8d99e6e5ce4c860cca54b
Bartosz Kalinowski <kelostrada@gmail.com>
2018-01-22 12:08:37 +0100

    simple navigation

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
 var photos = [];
-/* Download images from flickr */
-$(document).ready(function(){
-    $.ajax({
-        url : 'http://wizjemalarskie.pl/photos.php',
-        method: 'get',
-        dataType: 'json',
-        success : function(data){
-            photos = data.photos.kopie;
-            showPhotos();
-        }
-    })
-});
-var showPhotos = function(){
-    $('.image-container').empty().justifiedImages({
-        images : photos,
-        rowHeight: 200,
-        maxRowHeight: 500,
-        thumbnailPath: function(photo, width, height){
-            var purl = photo.url;
-            return purl;
-        },
-        getSize: function(photo){
-            return {width: photo.width, height: photo.height};
-        },
-        margin: $('#margin').val()
+var category = 'kopie';
+
+$(document).ready(function() {
+  $('.nav li').each(function(d, v) {
+    var $li = $(v);
+    var id = $li.attr('id');
+    $li.on('click', function() {
+      category = id;
+      $('.nav li a').removeClass('active');
+      $li.find('a').addClass('active');
+      showPhotos();
     });
+  })
+
+  $.ajax({
+    url: 'http://wizjemalarskie.pl/photos.php',
+    method: 'get',
+    dataType: 'json',
+    success: function(data) {
+      photos = data.photos;
+      showPhotos();
+    }
+  })
+});
+
+var showPhotos = function() {
+  $('.image-container').empty().justifiedImages({
+    images: photos[category],
+    rowHeight: 200,
+    maxRowHeight: 500,
+    thumbnailPath: function(photo, width, height) {
+      var purl = photo.url;
+      return purl;
+    },
+    getSize: function(photo) {
+      return {
+        width: photo.width,
+        height: photo.height
+      };
+    },
+    margin: $('#margin').val()
+  });
 };
-$( window ).resize(function() {
-    showPhotos();
+
+$(window).resize(function() {
+  showPhotos();
 });
